5fe66c8b4731e6b1ce3f5d98242665120a60b482
Daniel Downes <danieldownescentennial@gmail.com>
2021-05-02 16:17:34 -0700

    Updated Plot scene
    Plot is now a TextureButton node, rather than a Area2D. The code for making an Area2D act like a button has been removed. Plot now has a hover texture, which displays a small graphic to give visual clarity to what the player is selecting. Added multiple functions to Plot, including fill_plot, clear_plot, and reset_plot.

--- Scenes/Plot.gd
+++ Scenes/Plot.gd
@@ -1,41 +1,52 @@
-extends Area2D
+extends TextureButton
 
-var mouse_in_plot = false
-export var crop_name = "Carrots"
-export var crop_amount = 5
+export var crop_name = ""
+export var crop_amount = 0
 
 var stage = 0
-export var grown_stage = 3
-export var time_between_stages = 1
+export var grown_stage = 0
+export var seconds_between_stages = 0
 var is_grown = false
 var stage_timer
 
 onready var plant_sprites = $PlantSprites
 
-# Functions to add:
-# clear_plot - remove crop
-# fill_plot - add crop
-# reset_plot - set stage to zero and restart timer
-
 func _ready():
 	create_stage_timer()
 	plant_sprites.frame = stage
+	clear_plot()
+	fill_plot("Carrots", 5, 3, 1)
 
-func _process(delta):
-	if (Input.is_action_just_pressed("click") and mouse_in_plot and is_grown):
-		harvest_plot()
-	
-func harvest_plot():
+func fill_plot(crop_name, crop_amount, grown_stage, seconds_between_stages):
+	self.crop_name = crop_name
+	self.crop_amount = crop_amount
+	self.grown_stage = grown_stage
+	self.seconds_between_stages = seconds_between_stages
+	create_stage_timer()
+
+func clear_plot():
+	crop_name = ""
+	crop_amount = 0
+	stage = 0
+	grown_stage = 0
+	seconds_between_stages = 0
+	is_grown = false
+	stage_timer.queue_free()
+
+func reset_plot():
 	stage = 0
 	is_grown = false
 	stage_timer.start()
 	plant_sprites.frame = 0
+	
+func harvest_plot():
+	reset_plot()
 	get_tree().call_group("resources", "add_to_resources", crop_name, crop_amount)
 
 func create_stage_timer():
 	stage_timer = Timer.new()
 	stage_timer.connect("timeout", self, "_on_Stage_Timer_timeout")
-	stage_timer.wait_time = time_between_stages
+	stage_timer.wait_time = seconds_between_stages
 	add_child(stage_timer)
 	stage_timer.start()
 
@@ -49,11 +60,9 @@ func increase_stage():
 	plant_sprites.frame = stage
 	check_if_grown()
 
-func _on_Plot_mouse_entered():
-	mouse_in_plot = true
-
-func _on_Plot_mouse_exited():
-	mouse_in_plot = false
+func _on_Plot_pressed():
+	if (is_grown):
+		harvest_plot()
 
 func _on_Stage_Timer_timeout():
 	increase_stage()
